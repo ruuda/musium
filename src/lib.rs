7ee5c6643b1c9a2068a3c41b31a85dfa81a2fe6d
Ruud van Asseldonk <dev@veniogames.com>
2017-09-04 22:06:18 +0200

    Initial commit
    The start of a new adventure ...
    
    This is the initial commit of Metaindex, an idea that has been running
    through my head for several months now. Metaindex will be a library that
    scans a directory of flac files, extracts the metadata, and writes a
    database of metadata with indexes to support efficient lookup. The idea
    is that this library could be the basis for a music player, but it will
    be useful for different purposes as well. I intend to use it for Fmcount
    for instance, where it would be useful just for determining track-album-
    artist relationships.
    
    My current idea is to have something with an on-disk format that can
    be mmapped. That way the memory footprint would be low on memory-
    constrained systems, and performance could be high when sufficient
    memory is available. In particular, I would like to be able to use
    this as a basis for a fast local music player. There are a few okay-ish
    music players out there, but most choke on a music library of my size
    (about 13k tracks currently). I also want to build a network music
    player that can run on a Raspberry Pi, so memory usage is a concern.
    Generating the index should ideally be possible on such a low-end
    device, and consuming it on there is a must-have. And the index should
    be portable as well, so I can just rsync it everywhere in case building
    it on a Raspberry Pi is too slow.
    
    So let's sum up the requirements:
    
     * Low memory footprint when reading the index.
    
     * Efficient lookup of track <-> album, album <-> artist, lookup by
       Musicbrainz id.
    
     * Efficient listing of all artists, of all albums by an artist, of all
       tracks in an album.
    
     * Efficient fuzzy search on track title, album title, and artist.
    
     * Portable across different machines by rsyncing.
    
     * Incremental build, no rescan of all files when one album has been
       added. A rebuild of some of the files in the index, after adding an
       album, is ok. But the music files themselves cannot be accessed.
    
     * Possible to build in a streaming manner under memory constraints.
    
    The latter two points can be achieved by building small indices in
    memory, flushing those to disk, and then having a merge operation that
    can be streamed. This would support both the "append a single album" use
    case, as well as "index a large library through divide and conquer".

---
+++ src/lib.rs
@@ -0,0 +1,90 @@
+// Metaindex -- A music metadata indexing library
+// Copyright 2017 Ruud van Asseldonk
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// A copy of the License has been included in the root of the repository.
+
+// TODO: Remove once near-stable.
+#![allow(dead_code)]
+
+extern crate claxon;
+extern crate walkdir;
+
+use std::io;
+use std::path::{Path, PathBuf};
+
+trait MetaIndex {
+    /// Returns the number of tracks in the index.
+    fn len(&self) -> usize;
+}
+
+pub enum Error {
+    /// An IO error during writing the index or reading the index or metadata.
+    IoError(io::Error),
+
+    /// An FLAC file to be indexed could not be read.
+    FormatError(claxon::Error),
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::IoError(err)
+    }
+}
+
+impl From<claxon::Error> for Error {
+    fn from(err: claxon::Error) -> Error {
+        match err {
+            claxon::Error::IoError(io_err) => Error::IoError(io_err),
+            _ => Error::FormatError(err),
+        }
+    }
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+struct MemoryMetaIndex {
+
+}
+
+impl MemoryMetaIndex {
+    /// Create an empty memory-backed metaindex.
+    ///
+    /// The empty index acts as a unit for combining with other indices.
+    pub fn new() -> MemoryMetaIndex {
+        MemoryMetaIndex { }
+    }
+
+    /// Index the given files, and store the index in the target directory.
+    ///
+    /// Although this streams most metadata to disk, a few parts of the index
+    /// have to be kept in memory for efficient sorting, so the paths iterator
+    /// should not yield *too* many elements.
+    pub fn from_paths<I>(paths: I) -> Result<MemoryMetaIndex>
+    where I: IntoIterator,
+          <I as IntoIterator>::Item: AsRef<Path> {
+        for path in paths {
+            let flac_reader = claxon::FlacReader::open(path.as_ref())?;
+        }
+
+        Ok(MemoryMetaIndex::new())
+    }
+}
+
+impl MetaIndex for MemoryMetaIndex {
+    fn len(&self) -> usize {
+        // TODO: Real impl
+        0
+    }
+}
+
+mod tests {
+    use super::{MetaIndex, MemoryMetaIndex};
+
+    #[test]
+    fn new_is_empty() {
+        let mi = MemoryMetaIndex::new();
+        assert_eq!(mi.len(), 0);
+    }
+}
