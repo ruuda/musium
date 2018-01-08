d82c60b391662130692835cd6f4dca6d68dab6b0
Ruud van Asseldonk <dev@veniogames.com>
2018-01-08 23:26:20 +0100

    Add a few endpoints for server

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,14 @@ use std::process;
 use std::ffi::OsStr;
 use std::path::PathBuf;
 
-use simple_server::Server;
+use simple_server::{Request, Response, ResponseBuilder, Server};
+
+fn route(request: Request<&[u8]>,
+         response: &mut ResponseBuilder)
+    // TODO: Try to get rid of the vec.
+    -> Result<Response<Vec<u8>>, simple_server::Error> {
+    Ok(response.body("Hi".as_bytes().to_vec())?)
+}
 
 fn main() {
     if env::args().len() < 2 {
@@ -36,7 +43,34 @@ fn main() {
     // Have a basic server to serve an API.
     let server = Server::new(|request, mut response| {
         println!("Request: {} {}", request.method(), request.uri());
-        Ok(response.body("Hi".as_bytes())?)
+        let parts: Vec<_> = request.uri().path().split('/').filter(|x| x.len() > 0).collect();
+
+        if parts.len() == 2 {
+            let section = parts[0];
+            let id = parts[1];
+            match section {
+                "track" if id.ends_with(".flac") => {
+                    Ok(response.body("serve raw track bytes".as_bytes())?)
+                }
+                "track" => {
+                    Ok(response.body("track metadata".as_bytes())?)
+                }
+                "album" if id.ends_with(".jpg") => {
+                    Ok(response.body("album cover art".as_bytes())?)
+                }
+                "album" => {
+                    Ok(response.body("album metadata".as_bytes())?)
+                }
+                "artist" => {
+                    Ok(response.body("artist metadata".as_bytes())?)
+                }
+                _ => {
+                    Ok(response.body("BAD REQUEST".as_bytes())?)
+                }
+            }
+        } else {
+            Ok(response.body("BAD REQUEST".as_bytes())?)
+        }
     });
     server.listen("0.0.0.0", "8233");
 }
