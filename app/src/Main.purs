af31028e8ce36b638a5dfd3cb1a2686e2bd35bfe
Ruud van Asseldonk <dev@veniogames.com>
2019-06-29 17:11:55 +0200

    Request all albums from app

--- app/src/Main.purs
+++ app/src/Main.purs
@@ -1,11 +1,26 @@
 module Main where
 
+import Affjax as Http
+import Affjax.ResponseFormat as Http.ResponseFormat
+import Data.Argonaut.Core as Json
+import Data.Either (Either (..))
 import Effect (Effect)
-import Effect.Console as Console
+import Effect.Aff (Aff)
+import Effect.Aff as Aff
+import Effect.Class.Console as Console
 import Prelude
+
+getAlbums :: Aff Unit
+getAlbums = do
+  response <- Http.get Http.ResponseFormat.json "http://localhost:8233/albums"
+  case response.body of
+    Left err -> Console.log $ "Failed to retrieve albums: " <> Http.printResponseFormatError err
+    Right json -> Console.log $ Json.stringify json
 
 greet :: String -> String
 greet name = "Hello, " <> name <> "!"
 
 main :: Effect Unit
-main = Console.log (greet "World")
+main = do
+  Console.log (greet "World")
+  Aff.launchAff_ getAlbums
