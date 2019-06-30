090bf7a3f210feded2240a4ad89b462455691c5d
Ruud van Asseldonk <dev@veniogames.com>
2019-06-30 22:45:55 +0200

    Render album and track list somewhat nicer

--- app/src/AlbumComponent.purs
+++ app/src/AlbumComponent.purs
@@ -9,12 +9,18 @@ import Data.Newtype (unwrap)
 import Data.Const (Const)
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.Core (ClassName (..))
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Prelude
 
 import Model (Album, Track (..))
 import Model as Model
+
+data TrackList
+  = Hidden
+  | Loading
+  | Open (Array Track)
 
 type State =
   { album  :: Album
@@ -52,18 +58,41 @@ render state =
         [ HP.src (Model.thumbUrl album.id)
         , HP.alt $ album.title <> " by " <> album.artist
         ]
-      , HH.strong_ [ HH.text album.title ]
-      , HH.text " "
-      , HH.span_ [ HH.text album.artist ]
+      , HH.div
+        [ HP.class_ (ClassName "album-header") ]
+        [ HH.span
+          [ HP.class_ (ClassName "title") ]
+          [ HH.text album.title ]
+        , HH.span
+          [ HP.class_ (ClassName "album-artist") ]
+          [ HH.text album.artist ]
+        ]
       ] <> case state.tracks of
         Nothing -> []
         Just tracks ->
-          [ HH.ul_ $ map renderTrack tracks ]
+          [ HH.ul
+            [ HP.class_ (ClassName "track-list") ]
+            (map renderTrack tracks)
+          ]
 
 renderTrack :: forall m. Track -> H.ComponentHTML Action () m
 renderTrack (Track track) =
-  HH.li_
-    [ HH.text track.title ]
+  let
+    span class_ content =
+      HH.span [HP.class_ (ClassName class_)] [content]
+  in
+    HH.li_
+      [ HH.div
+        [ HP.class_ (ClassName "track-duration") ]
+        [ span "track" $ HH.text $ show track.trackNumber
+        , span "duration" $ HH.text $ show track.durationSeconds
+        ]
+      , HH.div
+        [ HP.class_ (ClassName "track-header") ]
+        [ span "title" $ HH.text track.title
+        , span "artist" $ HH.text track.artist
+        ]
+      ]
 
 handleAction :: forall o m. MonadAff m => Action -> H.HalogenM State Action () o m Unit
 handleAction = case _ of
