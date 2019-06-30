4e38920d21a3849fe31f67b2b3b79f5c6e8d7e8d
Ruud van Asseldonk <dev@veniogames.com>
2019-06-30 23:40:16 +0200

    Add headers to Purescript sources

--- app/src/AlbumComponent.purs
+++ app/src/AlbumComponent.purs
@@ -1,3 +1,10 @@
+-- Mindec -- Music metadata indexer
+-- Copyright 2019 Ruud van Asseldonk
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- A copy of the License has been included in the root of the repository.
+
 module AlbumComponent
   ( Slot
   , component
