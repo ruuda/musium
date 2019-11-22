a20884f450725ca7b2876288c7abc27a709df0e8
Ruud van Asseldonk <dev@veniogames.com>
2019-11-22 23:40:43 +0100

    Upgrade to latest commit in nixos-unstable channel
      curl                 7.65.0   -> 7.66.0
      gcc                  7.4.0    -> 8.3.0
      libssh2              1.8.2    -> 1.9.0
      libyaml              0.2.1    -> 0.2.2
      nghttp2              1.39.1   -> 1.39.2
      openssl              1.0.2s   -> 1.1.1d
      purescript           0.13.0   -> 0.13.4
      python               2.7.16   -> 2.7.17
      python2.7-Jinja2     2.10.1   -> 2.10.3
      python2.7-PyYAML     5.1      -> 5.1.2
      python2.7-certifi    2019.3.9 -> 2019.9.11
      python2.7-futures    3.2.0    -> 3.3.0
      python2.7-setuptools 41.0.1   -> 41.4.0
      rustup               1.18.3   -> 1.20.2
      sqlite               3.28.0   -> 3.30.0

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/73392e79aa62e406683d6a732eb4f4101f4732be.tar.gz";
-  sha256 = "049fq37sxdn5iis7ni407j2jsfrxbb41pgv5zawi02qj47f74az9";
+  url = "https://github.com/NixOS/nixpkgs/archive/e89b21504f3e61e535229afa0b121defb52d2a50.tar.gz";
+  sha256 = "0jqcv3rfki3mwda00g66d27k6q2y7ca5mslrnshfpbdm7j8ya0kj";
 }
