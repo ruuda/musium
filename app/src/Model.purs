1ba996426d6e29631b922ade459af6a266285ee6
Ruud van Asseldonk <dev@veniogames.com>
2019-12-01 21:29:35 +0100

    Define search function in the client

--- app/src/Model.purs
+++ app/src/Model.purs
@@ -29,11 +29,10 @@ import Control.Monad.Error.Class (class MonadThrow, throwError)
 import Data.Argonaut.Decode (decodeJson, getField) as Json
 import Data.Argonaut.Decode.Class (class DecodeJson)
 import Data.Either (Either (..))
-import Data.Newtype (class Newtype)
 import Data.String as String
 import Effect.Aff (Aff)
 import Effect.Exception (Error, error)
-import Data.Int (quot, rem)
+import Data.Int (rem)
 
 fatal :: forall m a. MonadThrow Error m => String -> m a
 fatal = error >>> throwError
@@ -71,8 +70,6 @@ newtype Album = Album
   , date :: String
   }
 
-derive instance newtypeAlbum :: Newtype Album _
-
 instance decodeJsonAlbum :: DecodeJson Album where
   decodeJson json = do
     obj        <- Json.decodeJson json
@@ -92,6 +89,57 @@ getAlbums = do
       Left err -> fatal $ "Failed to parse albums: " <> err
       Right albums -> pure $ sortWith (\(Album a) -> a.date) albums
 
+newtype SearchAlbum = SearchAlbum
+  { id :: AlbumId
+  , title :: String
+  , artist :: String
+  }
+
+newtype SearchTrack = SearchTrack
+  { id :: TrackId
+  , title :: String
+  , artist :: String
+  , album :: String
+  }
+
+newtype SearchResults = SearchResults
+  { albums :: Array SearchAlbum
+  , tracks :: Array SearchTrack
+  }
+
+instance decodeJsonSearchAlbum :: DecodeJson SearchAlbum where
+  decodeJson json = do
+    obj        <- Json.decodeJson json
+    id         <- map AlbumId $ Json.getField obj "id"
+    title      <- Json.getField obj "title"
+    artist     <- Json.getField obj "artist"
+    pure $ SearchAlbum { id, title, artist }
+
+instance decodeJsonSearchTrack :: DecodeJson SearchTrack where
+  decodeJson json = do
+    obj        <- Json.decodeJson json
+    id         <- map TrackId $ Json.getField obj "id"
+    title      <- Json.getField obj "title"
+    artist     <- Json.getField obj "artist"
+    album      <- Json.getField obj "album"
+    pure $ SearchTrack { id, title, artist, album }
+
+instance decodeJsonSearchResults :: DecodeJson SearchResults where
+  decodeJson json = do
+    obj    <- Json.decodeJson json
+    albums <- Json.getField obj "albums"
+    tracks <- Json.getField obj "tracks"
+    pure $ SearchResults { albums, tracks }
+
+search :: String -> Aff SearchResults
+search query = do
+  response <- Http.get Http.ResponseFormat.json ("/search?q=" <> query)
+  case response.body of
+    Left err -> fatal $ "Search failed: " <> Http.printResponseFormatError err
+    Right json -> case Json.decodeJson json of
+      Left err -> fatal $ "Failed to parse search results: " <> err
+      Right results -> pure results
+
 newtype Track = Track
   { id :: TrackId
   , discNumber :: Int
@@ -100,8 +148,6 @@ newtype Track = Track
   , artist :: String
   , durationSeconds :: Int
   }
-
-derive instance newtypeTrack :: Newtype Track _
 
 instance decodeJsonTrack :: DecodeJson Track where
   decodeJson json = do
