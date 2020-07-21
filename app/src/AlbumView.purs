db7aa01cdc802aa53d0c33149e231f93be40cc49
Ruud van Asseldonk <dev@veniogames.com>
2020-07-22 00:56:17 +0200

    Restore layout after changing ids

--- app/src/AlbumView.purs
+++ app/src/AlbumView.purs
@@ -21,41 +21,39 @@ import Model (Album (..), Track (..))
 import Model as Model
 
 renderAlbum :: Album -> Html Unit
-renderAlbum (Album album) =
+renderAlbum (Album album) = do
   Html.div $ do
-    Html.setId "album-view"
+    Html.addClass "album-info"
     Html.div $ do
-      Html.addClass "album-info"
-      Html.div $ do
-        Html.addClass "cover"
-        let alt = album.title <> " by " <> album.artist
-        -- Add 3 images: a blurred backdrop, the low-resolution thumbnail that
-        -- should already be cached for quick display, and the high-resolution
-        -- cover art on top of that.
-        Html.img (Model.thumbUrl album.id) alt $ Html.addClass "backdrop"
-        Html.img (Model.thumbUrl album.id) alt $ pure unit
-        Html.img (Model.coverUrl album.id) alt $ pure unit
-      Html.hgroup $ do
-        Html.h1 $ Html.text album.title
-        Html.h2 $ do
-          Html.span $ do
-            Html.addClass "artist"
-            Html.text album.artist
-          Html.text " ⋅ "
-          Html.span $ do
-            Html.addClass "date"
-            Html.text album.date
+      Html.addClass "cover"
+      let alt = album.title <> " by " <> album.artist
+      -- Add 3 images: a blurred backdrop, the low-resolution thumbnail that
+      -- should already be cached for quick display, and the high-resolution
+      -- cover art on top of that.
+      Html.img (Model.thumbUrl album.id) alt $ Html.addClass "backdrop"
+      Html.img (Model.thumbUrl album.id) alt $ pure unit
+      Html.img (Model.coverUrl album.id) alt $ pure unit
+    Html.hgroup $ do
+      Html.h1 $ Html.text album.title
+      Html.h2 $ do
+        Html.span $ do
+          Html.addClass "artist"
+          Html.text album.artist
+        Html.text " ⋅ "
+        Html.span $ do
+          Html.addClass "date"
+          Html.text album.date
 
-    trackList <- Html.ul $ do
-      Html.addClass "track-list"
-      ask
+  trackList <- Html.ul $ do
+    Html.addClass "track-list"
+    ask
 
-    liftEffect $ launchAff_ $ do
-      tracks <- Model.getTracks album.id
-      let
-        lis :: Html Unit
-        lis = traverse_ (renderTrack $ Album album) tracks
-      liftEffect $ Html.withElement trackList lis
+  liftEffect $ launchAff_ $ do
+    tracks <- Model.getTracks album.id
+    let
+      lis :: Html Unit
+      lis = traverse_ (renderTrack $ Album album) tracks
+    liftEffect $ Html.withElement trackList lis
 
 renderTrack :: Album -> Track -> Html Unit
 renderTrack album (Track track) =
