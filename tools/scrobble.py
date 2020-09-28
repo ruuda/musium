222c5bf5964e2e54acd3f5d308bc378dae29b597
Ruud van Asseldonk <dev@veniogames.com>
2020-09-28 22:41:36 +0200

    Authenticate with Last.fm API
    I am now able to make an API call, however, my scrobble is rejected with
    code 1 ("Artist was ignored"). I suppose this is because I have a feat.
    artist in there, and Last.fm does not like to have these in artists. I
    can try including a MusicBrainz id, perhaps that will help.

--- tools/scrobble.py
+++ tools/scrobble.py
@@ -11,18 +11,39 @@
 scrobble.py -- Scrobble the play log to Last.fm.
 
 Usage:
-    scrobble.py plays.log
+
+    scrobble.py authenticate
+    scrobble.py scrobble plays.log
+
+The following environment variables are expected to be set:
+
+    LAST_FM_API_KEY       Last.fm API key.
+    LAST_FM_SECRET        Associated shared secret.
+    LAST_FM_SESSION_KEY   Printed by authenticate, only needed for scrobble.
+
+You can create an API key and secret at https://www.last.fm/api/account/create.
+
 """
 
 from __future__ import annotations
 
-import sys
+import hashlib
 import json
+import os
+import sys
+import urllib
 
 from dataclasses import dataclass
 from datetime import datetime
 from enum import Enum
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
 from typing import Dict, Union, Iterator
+
+
+API_KEY = os.getenv('LAST_FM_API_KEY')
+SECRET = os.getenv('LAST_FM_SECRET')
+SESSION_KEY = os.getenv('LAST_FM_SESSION_KEY')
 
 
 class EventType(Enum):
@@ -60,6 +81,32 @@ class Event:
         }
         return Event(**args)
 
+    def format_scrobble(self, index: int) -> Dict[str, str]:
+        """
+        Format a completed event as parameters for a form/url-encoded request
+        to scrobble the track.
+        """
+        assert self.event == EventType.started
+
+        def indexed(key: str) -> str:
+            return f'{key}[{index}]'
+
+        result = {
+            indexed('artist'): self.artist,
+            indexed('track'): self.title,
+            indexed('timestamp'): str(int(self.time.timestamp())),
+            indexed('album'): self.album,
+            indexed('trackNumber'): str(self.track_number),
+            indexed('duration'): str(self.duration_seconds),
+        }
+
+        # Last.fm says "The album artist - if this differs from the track artist."
+        # My interpretation is that we shouldn't include it unless it differs.
+        if self.album_artist != self.artist:
+            result[indexed('albumArtist')] = self.album_artist
+
+        return result
+
 
 def read_play_log(fname: str) -> Iterator[Event]:
     with open(fname, 'r', encoding='utf-8') as f:
@@ -69,7 +116,7 @@ def read_play_log(fname: str) -> Iterator[Event]:
 
 def events_to_scrobble(events: Iterator[Event]) -> Iterator[Event]:
     """
-    Return the completed event of listens to scrobble.
+    Return the started event of listens to scrobble.
     * Match up started and completed events.
     * Apply Last.fm requirements for when to scrobble.
     """
@@ -94,21 +141,127 @@ def events_to_scrobble(events: Iterator[Event]) -> Iterator[Event]:
             # The playtime requirement is implied by the above check.
             and event.duration_seconds > 30
         ):
-            yield event
+            # We yield the started event, not the completion event, because for
+            # a scrobble we need the time at which the track started playing.
+            yield prev_event
 
         prev_event = event
 
 
-def main(play_log: str) -> None:
+def iter_chunks(events: Iterator[Event], n: int) -> Iterator[List[Event]]:
+    """
+    Yield chunks of n items from the original iterator.
+    The last chunk may be smaller.
+    """
+    result = []
+    for event in events:
+        result.append(event)
+
+        if len(result) == n:
+            yield result
+            result = []
+
+    if len(result) > 0:
+        yield result
+
+
+def format_batch_request(events: List[Event]) -> Request:
+    """
+    Format a POST request to scrobble the given events.
+    """
+    assert len(events) <= 50, 'Last.fm allows at most 50 scrobbles per batch.'
+
+    params = {
+        'method': 'track.scrobble',
+        'sk': SESSION_KEY,
+    }
+
+    for i, event in enumerate(events):
+        params.update(event.format_scrobble(i))
+
+    return format_signed_request(http_method='POST', data=params)
+
+
+def format_signed_request(
+    http_method: str,
+    data: Dict[str, str],
+) -> Request:
+    """
+    Format a signed request to
+    """
+    params = {
+        **data,
+        'api_key': API_KEY,
+    }
+
+    # Sort alphabetically by key, as required for the signature.
+    params = {k: v for k, v in sorted(params.items())}
+
+    sign_input = ''.join(f'{k}{v}' for k, v in params.items()) + SECRET
+    params['api_sig'] = hashlib.md5(sign_input.encode('utf-8')).hexdigest()
+
+    # The "format" key is not part of the signature input, we need to add it
+    # later.
+    params['format'] = 'json'
+
+    # Encode the parameters as key=value separated by ampersands, percent-escape
+    # characters where necessary. Encode space as %20, do escape the slash by
+    # marking no character as safe.
+    body_str = urlencode(params, quote_via=urllib.parse.quote, safe='')
+
+    return Request(
+        url='https://ws.audioscrobbler.com/2.0/',
+        method=http_method,
+        data=body_str.encode('utf-8'),
+    )
+
+
+def cmd_scrobble(play_log: str) -> None:
     events = read_play_log(play_log)
     scrobble_events = events_to_scrobble(events)
-    for event in scrobble_events:
-        print(event)
+
+    # Last.fm allows submitting batches of at most 50 scrobbles at once.
+    for batch in iter_chunks(scrobble_events, n=50):
+        req = format_batch_request(batch)
+        response = json.load(urlopen(req))
+        print(response)
+
+
+def cmd_authenticate() -> None:
+    req = format_signed_request(
+        http_method='GET',
+        data={'method': 'auth.getToken'},
+    )
+    response = json.load(urlopen(req))
+    token = response['token']
+
+    print('Please authorize the application at the following page:\n')
+    print(f'https://www.last.fm/api/auth/?api_key={API_KEY}&token={token}\n')
+    input('Press Enter to continue.')
+
+    req = format_signed_request(
+        http_method='GET',
+        data={'method': 'auth.getSession', 'token': token},
+    )
+    response = json.load(urlopen(req))
+    username = response['session']['name']
+    session_key = response['session']['key']
+    print(f'\nScrobbling authorized by user {username}.')
+    print('Please set the following environment variable when scrobbling:\n')
+    print(f'LAST_FM_SESSION_KEY={session_key}')
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    command = ''
+    if len(sys.argv) > 1:
+        command = sys.argv[1]
+
+    if command == 'authenticate' and len(sys.argv) == 2:
+        cmd_authenticate()
+
+    elif command == 'scrobble' and len(sys.argv) == 3:
+        cmd_scrobble(sys.argv[2])
+
+    else:
         print(__doc__)
         sys.exit(1)
-    else:
-        main(sys.argv[1])
