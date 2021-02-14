22f888d81ed0da3c9f5c6644dff4c5b0d262f9d4
Ruud van Asseldonk <dev@veniogames.com>
2021-02-14 12:57:03 +0100

    Add comparison mode to benchmark script
    This performs a Mann-Whitney U test on the before and after data. The
    Mann-Whitney U test is great for benchmarks, because unlike the
    Student's T test, it does not assume that the underlying distribution is
    normal. A normal distribution is a questionable assumption for
    benchmarks, because things like interrupts and mispredicts cause delays;
    there is never a speedup over the minimal running time. If you model
    those delays with a Beta distribution, the sum of many delays becomes a
    Gamma distribution, and as the number of delays grows, it does tend
    towards a normal distribution, but it's not clear to me how close this
    is. Also, I spent a lot of time analyzing benchmark data, and even
    assuming that delays follow a Beta distribution is already questionable.
    
    The Mann-Whitney U test works under much weaker assumptions. The
    trade-off is that it has less power for a given number of samples, if
    the distribution *is* actually normal. I think this is fine, because
    benchmarks are automated, we can just collect more samples.

--- tools/benchmark.py
+++ tools/benchmark.py
@@ -13,15 +13,19 @@ benchmark.py -- Benchmark Musium indexing performance.
 Usage:
 
     tools/benchmark.py record <n_iters> <outfile> <command...>
+    tools/benchmark.py compare <before> <after>
 
 Example:
 
     tools/benchmark.py record 50 before.tsv target/release/musium cache musium.conf
+    tools/benchmark.py compare before.tsv after.tsv
 """
 
-import statistics
 import subprocess
 import sys
+
+import numpy as np
+import scipy.stats as stats
 
 from typing import List, NamedTuple
 
@@ -75,7 +79,7 @@ def perf_stat(command: List[str]) -> None:
     return Stat(cycles, instructions, seconds_elapsed, seconds_user, seconds_sys)
 
 
-def main(n_iters: int, outfile: str, command: List[str]) -> None:
+def record(n_iters: int, outfile: str, command: List[str]) -> None:
     stats: List[Stat] = []
     for i in range(n_iters):
         print(f'\r[{i+1} / {n_iters}] Running {command}', end='', flush=True)
@@ -87,18 +91,110 @@ def main(n_iters: int, outfile: str, command: List[str]) -> None:
             f.write('\t'.join(s) + '\n')
 
     print(f'\nResults written to {outfile}.')
-    print('Median instructions:', statistics.median(int(s.instructions) for s in stats))
-    print('Median seconds:     ', statistics.median(float(s.seconds_elapsed) for s in stats))
+
+
+def load_tsv(fname: str) -> List[Stat]:
+    with open(fname, 'r', encoding='utf-8') as f:
+        lines = iter(f)
+        # Read the first line and check that the header is as expected.
+        # Cut off the trailing newline.
+        header = Stat(*next(lines)[:-1].split('\t'))
+        assert header == Stat(
+            cycles='cycles',
+            instructions='instructions',
+            seconds_elapsed='seconds_elapsed',
+            seconds_user='seconds_user',
+            seconds_sys='seconds_sys',
+        )
+
+        return [Stat(*line[:-1].split('\t')) for line in lines]
+
+
+def print_u_test(befores: np.array, afters: np.array) -> None:
+    # Perform a two-sided Mann-Whitney U-test to test the following null
+    # hypothesis: `befores` and `afters` are two samples drawn from the same
+    # population. The alternative hypothesis, is that they are drawn from a
+    # different distribution. The Mann-Whitney U-test is similar to
+    # Student's t-test, but it is more robust to outliers, because it is based
+    # on relative ranking, rather than mean and variance.
+    test = stats.mannwhitneyu(befores, afters, alternative='two-sided')
+
+    # The U-statistic is defined as the number of times that a value from
+    # `afters` exceeds a value from `befores` (with 0.5 for ties), therefore
+    # the maximal value is the product of the lengths. The quotient of the
+    # U-statistic and its maximal value is also called the "effect size". A
+    # value of 1 means that all `befores` were smaller than all `afters`, a
+    # value of 0 means that all `afters` were smaller than all `befores`.
+    max_statistic = float(len(befores) * len(afters))
+    effect_size = 1.0 - test.statistic / max_statistic
+
+    # The p-value is the probability of observing an effect size at least as
+    # far from 0.5 as we did, under the assumption that the null hypothesis
+    # is true.
+    print('  Mann–Whitney U test')
+    print('  null hypothesis: distributions before and after are equal')
+    print(f'  effect size:    {effect_size:>10.7f}')
+    print(f'  p-value:        {test.pvalue:>10.7f}')
+
+    # Measurements are automated and fast; we can have much higher standards
+    # than p=0.05 here.
+    if test.pvalue < 0.0001:
+        # If the effect is significant enough, also print the differences and
+        # ratios of the means, together with an estimate of the error in those,
+        # based on the MAD (median absolute deviation) and standard error
+        # propagation formulas.
+        med_before = np.median(befores)
+        med_after = np.median(afters)
+        mad_before = np.median(np.abs(befores - med_before))
+        mad_after = np.median(np.abs(afters - med_after))
+
+        diff = med_after - med_before
+        # The squared error in A + B is the sum of the squared errors.
+        diff_err = np.sqrt(np.square(mad_before) + np.square(mad_after))
+
+        ratio = med_after / med_before
+        # The relative squared error in A / B is the sum of the squared relative
+        # errors.
+        ratio_err = ratio * np.sqrt(
+            np.square(mad_before / med_before) + np.square(mad_after / med_after)
+        )
+
+        print(f'  after - before: {diff:>10,.3f} ± {diff_err:.3f}')
+        print(f'  after / before: {ratio:>10.3f} ± {ratio_err:.3f}')
+
+    else:
+        print('  insufficient evidence to reject the null hypothesis')
+
+
+def compare(before_file: str, after_file: str) -> None:
+    befores = load_tsv(before_file)
+    afters = load_tsv(after_file)
+
+    print('Instructions\n')
+    print_u_test(
+        np.array([float(s.instructions) for s in befores]),
+        np.array([float(s.instructions) for s in afters]),
+    )
+
+    print('\nSeconds elapsed\n')
+    print_u_test(
+        np.array([float(s.seconds_elapsed) for s in befores]),
+        np.array([float(s.seconds_elapsed) for s in afters]),
+    )
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 5:
+    if len(sys.argv) < 4:
         print(__doc__)
         sys.exit(1)
     else:
         cmd = sys.argv[1]
         if cmd == 'record':
-            main(int(sys.argv[2]), sys.argv[3], sys.argv[4:])
+            record(int(sys.argv[2]), sys.argv[3], sys.argv[4:])
+
+        elif cmd == 'compare':
+            compare(sys.argv[2], sys.argv[3])
+
         else:
             print('Invalid command:', cmd)
             sys.exit(1)
