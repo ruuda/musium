ea684501bba0c48509d5a690c50a1bf9f58a0c32
Ruud van Asseldonk <dev@veniogames.com>
2021-05-31 22:44:47 +0200

    Make Database type wrap all queries
    Previously the database logic was limited to the history thread, so it
    lived there. But now that I want to do more with it, I think I'll go
    with a setup where the Database type has a mid-level Rust interface for
    all of the queries it supports. With mid-level I mean that it does
    accept Musium primitive types like album and track ids, but not
    higher-level data structures like an index where we have to look up
    string refs. That can be done at the call site. This way the Database
    type can remain a relatively simple wrapper around the queries.
    
    I am a bit anxious about passing so many arguments by position, the
    probability of making a mistake is high ... but if I got it wrong, it
    is will be easy to correct.
    
    I should really add tests, but the listens aren't read from the Rust
    side, so that would create even more boilerplate. For the new files
    table, roundtrip tests would be good to have though.

--- src/database.rs
+++ src/database.rs
@@ -9,17 +9,25 @@
 
 use sqlite;
 
+use crate::player::QueueId;
+use crate::prim::{AlbumId, ArtistId, TrackId};
+
 pub type Result<T> = sqlite::Result<T>;
+
+/// Row id of a row in the `listens` table.
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub struct ListenId(i64);
 
 /// Wraps the SQLite connection with some things to manipulate the DB.
 pub struct Database<'conn> {
     pub connection: &'conn sqlite::Connection,
     pub insert_started: sqlite::Statement<'conn>,
     pub update_completed: sqlite::Statement<'conn>,
-    pub last_insert_id: Option<i64>,
+    pub insert_file: sqlite::Statement<'conn>,
+    pub get_file_mtime: sqlite::Statement<'conn>,
 }
 
-fn create_schema(connection: &sqlite::Connection) -> Result<()> {
+pub fn ensure_schema_exists(connection: &sqlite::Connection) -> Result<()> {
     connection.execute(
         "
         create table if not exists listens
@@ -55,7 +63,7 @@ fn create_schema(connection: &sqlite::Connection) -> Result<()> {
         -- NULL if the track has not been scrobbled by us.
         , scrobbled_at     string  null     check (started_at < scrobbled_at)
         );
-        "
+        ",
     )?;
 
     // We can record timestamps in sub-second granularity, but external systems
@@ -67,7 +75,7 @@ fn create_schema(connection: &sqlite::Connection) -> Result<()> {
         "
         create unique index if not exists ix_listens_unique_second
         on listens (cast(strftime('%s', started_at) as integer));
-        "
+        ",
     )?;
 
     // Next is the table with tag data. This is the raw data extracted from
@@ -107,54 +115,171 @@ fn create_schema(connection: &sqlite::Connection) -> Result<()> {
         , tag_bs17704_track_loudness     string
         , tag_bs17704_album_loudness     string
         );
-        "
+        ",
     )?;
 
     Ok(())
 }
 
-/// Ensure that all tables exist, prepare statements.
-pub fn initialize_db(connection: &sqlite::Connection) -> Result<Database> {
-    create_schema(connection)?;
+impl<'conn> Database<'conn> {
+    /// Prepare statements.
+    ///
+    /// Does not ensure that all tables exist, use [`create_schema`] for that.
+    pub fn new(connection: &sqlite::Connection) -> Result<Database> {
+        let insert_started = connection.prepare(
+            "
+            insert into listens
+            ( started_at
+            , queue_id
+            , track_id
+            , album_id
+            , album_artist_id
+            , track_title
+            , album_title
+            , track_artist
+            , album_artist
+            , duration_seconds
+            , track_number
+            , disc_number
+            , source
+            )
+            values
+            ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 'musium');
+            ",
+        )?;
 
-    let insert_started = connection.prepare(
-        "
-        insert into listens
-        ( started_at
-        , queue_id
-        , track_id
-        , album_id
-        , album_artist_id
-        , track_title
-        , album_title
-        , track_artist
-        , album_artist
-        , duration_seconds
-        , track_number
-        , disc_number
-        , source
-        )
-        values
-        ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 'musium');
-        "
-    )?;
+        let update_completed = connection.prepare(
+            "
+            update listens
+              set completed_at = ?
+            where
+              id = ?
+              and queue_id = ?
+              and track_id = ?;
+            ",
+        )?;
 
-    let update_completed = connection.prepare(
-        "
-        update listens
-          set completed_at = ?
-        where
-          id = ?
-          and queue_id = ?
-          and track_id = ?;
-        "
-    )?;
+        let insert_file = connection.prepare(
+            "
+            insert into files
+            ( filename
+            , mtime
+            , streaminfo_channels
+            , streaminfo_bits_per_sample
+            , streaminfo_samples
+            , streaminfo_sample_rate
+            , tag_album
+            , tag_albumartist
+            , tag_albumartistsort
+            , tag_musicbrainz_albumartistid
+            , tag_musicbrainz_albumid
+            , tag_musicbrainz_trackid
+            , tag_discnumber
+            , tag_tracknumber
+            , tag_originaldate
+            , tag_date
+            , tag_title
+            , tag_bs17704_track_loudness
+            , tag_bs17704_album_loudness
+            , queue_id
+            , track_id
+            , album_id
+            , album_artist_id
+            , track_title
+            , album_title
+            , track_artist
+            , album_artist
+            , duration_seconds
+            , track_number
+            , disc_number
+            , source
+            )
+            values
+            ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+            , ?, ?, ?, ?, ?, ?, ?, ?
+            );
+            ",
+        )?;
 
-    let result = Database {
-        connection: connection,
-        insert_started: insert_started,
-        update_completed: update_completed,
-        last_insert_id: None,
-    };
-    Ok(result)
+        let get_file_mtime = connection.prepare(
+            "
+            select mtime from files where filename = ?;
+            ",
+        )?;
+
+        let result = Database {
+            connection: connection,
+            insert_started: insert_started,
+            update_completed: update_completed,
+            insert_file: insert_file,
+            get_file_mtime: get_file_mtime,
+        };
+
+        Ok(result)
+    }
+
+    /// Insert a listen into the "listens" table, return its row id.
+    pub fn insert_listen_started(
+        &mut self,
+        started_at: &str,
+        queue_id: QueueId,
+        track_id: TrackId,
+        album_id: AlbumId,
+        album_artist_id: ArtistId,
+        track_title: &str,
+        track_artist: &str,
+        album_title: &str,
+        album_artist: &str,
+        duration_seconds: u16,
+        track_number: u8,
+        disc_number: u8,
+    ) -> Result<ListenId> {
+        self.insert_started.reset()?;
+        self.insert_started.bind(1, started_at)?;
+        self.insert_started.bind(2, queue_id.0 as i64)?;
+        self.insert_started.bind(3, track_id.0 as i64)?;
+        self.insert_started.bind(4, album_id.0 as i64)?;
+        self.insert_started.bind(5, album_artist_id.0 as i64)?;
+        self.insert_started.bind(6, track_title)?;
+        self.insert_started.bind(7, track_artist)?;
+        self.insert_started.bind(8, album_title)?;
+        self.insert_started.bind(9, album_artist)?;
+        self.insert_started.bind(10, duration_seconds as i64)?;
+        self.insert_started.bind(11, track_number as i64)?;
+        self.insert_started.bind(12, disc_number as i64)?;
+
+        let result = self.insert_started.next()?;
+        // This query returns no rows, it should be done immediately.
+        assert_eq!(result, sqlite::State::Done);
+
+        // The "sqlite" crate does not have a wrapper for this function.
+        let id = unsafe {
+            sqlite3_sys::sqlite3_last_insert_rowid(self.connection.as_raw())
+        } as i64;
+
+        Ok(ListenId(id))
+    }
+
+    /// Update the completed time of a previously inserted listen.
+    ///
+    /// Also takes the queue id and track id as a sanity check.
+    pub fn update_listen_completed(
+        &mut self,
+        listen_id: ListenId,
+        completed_time: &str,
+        queue_id: QueueId,
+        track_id: TrackId,
+    ) -> Result<()> {
+        self.update_completed.reset()?;
+        self.update_completed.bind(1, completed_time)?;
+        self.update_completed.bind(2, listen_id.0)?;
+        self.update_completed.bind(3, queue_id.0 as i64)?;
+        self.update_completed.bind(4, track_id.0 as i64)?;
+
+        let result = self.update_completed.next()?;
+        // This query returns no rows, it should be done immediately.
+        assert_eq!(result, sqlite::State::Done);
+
+        Ok(())
+    }
 }
--- src/history.rs
+++ src/history.rs
@@ -10,102 +10,15 @@
 use std::path::{PathBuf};
 use std::sync::mpsc::Receiver;
 use sqlite;
-use sqlite3_sys;
 
 use crate::{MetaIndex, TrackId};
 use crate::player::QueueId;
-use crate::database::{Database, Result, initialize_db};
+use crate::database::{Database, ensure_schema_exists};
 
 /// Changes in the playback state to be recorded.
 pub enum PlaybackEvent {
     Started(QueueId, TrackId),
     Completed(QueueId, TrackId),
-}
-
-/// Insert a new row into the "listens" table.
-fn insert_started(
-    db: &mut Database,
-    index: &dyn MetaIndex,
-    time: chrono::DateTime<chrono::Utc>,
-    queue_id: QueueId,
-    track_id: TrackId,
-) -> Result<()> {
-    let use_zulu_suffix = true;
-    let time_str = time.to_rfc3339_opts(chrono::SecondsFormat::Millis, use_zulu_suffix);
-    let track = index.get_track(track_id).unwrap();
-    let album = index.get_album(track.album_id).unwrap();
-    let artist = index.get_artist(album.artist_id).unwrap();
-
-    db.insert_started.reset()?;
-    db.insert_started.bind(1, &time_str[..])?;
-    db.insert_started.bind(2, queue_id.0 as i64)?;
-    db.insert_started.bind(3, track_id.0 as i64)?;
-    db.insert_started.bind(4, track.album_id.0 as i64)?;
-    db.insert_started.bind(5, album.artist_id.0 as i64)?;
-    db.insert_started.bind(6, index.get_string(track.title))?;
-    db.insert_started.bind(7, index.get_string(album.title))?;
-    db.insert_started.bind(8, index.get_string(track.artist))?;
-    db.insert_started.bind(9, index.get_string(artist.name))?;
-    db.insert_started.bind(10, track.duration_seconds as i64)?;
-    db.insert_started.bind(11, track.track_number as i64)?;
-    db.insert_started.bind(12, track.disc_number as i64)?;
-
-    let result = db.insert_started.next()?;
-    // This query returns no rows, it should be done immediately.
-    assert_eq!(result, sqlite::State::Done);
-
-    // The "sqlite" crate does not have a wrapper for this function.
-    let id = unsafe {
-        sqlite3_sys::sqlite3_last_insert_rowid(db.connection.as_raw())
-    } as i64;
-
-    db.last_insert_id = Some(id);
-    Ok(())
-}
-
-/// Update a row to insert the completed time.
-fn update_completed(
-    db: &mut Database,
-    row_id: i64,
-    time: chrono::DateTime<chrono::Utc>,
-    queue_id: QueueId,
-    track_id: TrackId,
-) -> Result<()> {
-    let use_zulu_suffix = true;
-    let time_str = time.to_rfc3339_opts(chrono::SecondsFormat::Millis, use_zulu_suffix);
-
-    db.update_completed.reset()?;
-    db.update_completed.bind(1, &time_str[..])?;
-    db.update_completed.bind(2, row_id)?;
-    db.update_completed.bind(3, queue_id.0 as i64)?;
-    db.update_completed.bind(4, track_id.0 as i64)?;
-
-    let result = db.update_completed.next()?;
-    // This query returns no rows, it should be done immediately.
-    assert_eq!(result, sqlite::State::Done);
-
-    Ok(())
-}
-
-fn append_event(
-    db: &mut Database,
-    index: &dyn MetaIndex,
-    event: PlaybackEvent,
-) -> Result<()> {
-    let now = chrono::Utc::now();
-
-    match event {
-        PlaybackEvent::Started(queue_id, track_id) => {
-            insert_started(db, index, now, queue_id, track_id)?;
-        }
-        PlaybackEvent::Completed(queue_id, track_id) => {
-            if let Some(row_id) = db.last_insert_id {
-                update_completed(db, row_id, now, queue_id, track_id)?;
-            }
-        }
-    }
-
-    Ok(())
 }
 
 /// Main for the thread that logs historical playback events.
@@ -115,11 +28,54 @@ pub fn main(
     events: Receiver<PlaybackEvent>,
 ) {
     let connection = sqlite::open(db_path).expect("Failed to open SQLite database.");
-    let mut db = initialize_db(&connection).expect("Failed to initialize SQLite database.");
+    ensure_schema_exists(&connection).expect("Failed to create schema in SQLite database.");
+    let mut db = Database::new(&connection).expect("Failed to prepare SQLite statements.");
+
+    let mut last_listen_id = None;
+
     for event in events {
-        match append_event(&mut db, index, event) {
-            Ok(()) => {},
-            Err(err) => eprintln!("Failed to write event to SQLite database: {}", err),
+        let now = chrono::Utc::now();
+        let use_zulu_suffix = true;
+        let now_str = now.to_rfc3339_opts(chrono::SecondsFormat::Millis, use_zulu_suffix);
+
+        match event {
+            PlaybackEvent::Started(queue_id, track_id) => {
+                let track = index.get_track(track_id).unwrap();
+                let album = index.get_album(track.album_id).unwrap();
+                let artist = index.get_artist(album.artist_id).unwrap();
+                let result = db.insert_listen_started(
+                    &now_str[..],
+                    queue_id,
+                    track_id,
+                    track.album_id,
+                    album.artist_id,
+                    index.get_string(track.title),
+                    index.get_string(album.title),
+                    index.get_string(track.artist),
+                    index.get_string(artist.name),
+                    track.duration_seconds,
+                    track.track_number,
+                    track.disc_number,
+                );
+                last_listen_id = Some(result.expect("Failed to insert listen started event into SQLite database."));
+            }
+            PlaybackEvent::Completed(queue_id, track_id) => {
+                if let Some(listen_id) = last_listen_id {
+                    db.update_listen_completed(
+                        listen_id,
+                        &now_str[..],
+                        queue_id,
+                        track_id,
+                    ).expect(
+                        "Failed to insert listen completed event into SQLite database."
+                    );
+                } else {
+                    panic!(
+                        "Completed queue entry {}, track {}, before starting.",
+                        queue_id, track_id,
+                    );
+                }
+            }
         }
     }
 }
