ff89679c3a0ab7e5ce543efa125a689d82ba57d3
Ruud van Asseldonk <dev@veniogames.com>
2022-01-08 18:19:34 +0100

    Handle U+309A in word splitter
    Today is the first time I encounter a non-Latin code point that is not
    alphanumeric. I can’t read Japanese anyway for for me it makes most
    sense to ignore this code point.

--- src/string_utils.rs
+++ src/string_utils.rs
@@ -143,7 +143,8 @@ pub fn normalize_words(title: &str, dest: &mut Vec<String>) {
     // is unlikely to contain a lot of information about the title. (Deadmau5
     // can go and use some normal titles next time.) We remove accents to make
     // searching easier without having to type the exact accent.
-    let drop = "“”‘’'\"`()[]«»,❦|\u{300}\u{301}\u{302}\u{303}\u{304}\u{306}\u{307}\u{308}\u{327}";
+    // U+309a is a Japanese diacritic.
+    let drop = "“”‘’'\"`()[]«»,❦|\u{300}\u{301}\u{302}\u{303}\u{304}\u{306}\u{307}\u{308}\u{327}\u{309a}";
     let keep = "$€#&=*%∆";
 
     // Cut words at the following punctuation characters, but still include them
