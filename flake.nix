0e7e1ef62b0bb72aa3bb9de686b030c55d6d2193
Ruud van Asseldonk <dev@veniogames.com>
2022-09-07 23:43:05 +0200

    Shoehorn things into the flake madness
    Why do I have to write this ridiculous "forEachSystem", or import it
    from this third-party flake, to make things barely usable? Why is the
    system in such an awkward location all the time? But anyway, it does
    work now.

--- flake.nix
+++ flake.nix
@@ -4,42 +4,49 @@
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
   inputs.querybinder.url = "github:ruuda/querybinder";
 
-  outputs = { self, nixpkgs, querybinder }: 
+  outputs = { self, nixpkgs, querybinder }:
     let
-      name = "musium";
-      version = builtins.substring 0 8 self.lastModifiedDate;
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      forAllNixpkgs = f: forAllSystems (system: f (import nixpkgs { inherit system; }));
+      supportedSystems = ["x86_64-linux" "aarch64-linux"];
+      # Ridiculous boilerplate required to make flakes somewhat usable.
+      forEachSystem = f:
+        builtins.zipAttrsWith
+          (name: values: builtins.foldl' (x: y: x // y) {} values)
+          (map
+            (k: builtins.mapAttrs (name: value: { "${k}" = value; }) (f k))
+            supportedSystems
+          );
     in
-      {
-        devShells = forAllNixpkgs (pkgs: {
-          default = pkgs.mkShell {
-            nativeBuildInputs = [
-              pkgs.mkdocs
-              pkgs.psc-package
-              pkgs.purescript
-              pkgs.rustup
-              pkgs.sqlite
-              # TODO: Don't hard-code the system name ...
-              querybinder.packages.x86_64-linux.default
-            ];
-          };
-        });
-
-        packages = forAllNixpkgs (pkgs: {
-          default = pkgs.rustPlatform.buildRustPackage {
-            inherit name version;
-            src = ./.;
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-              outputHashes = {
-                "claxon-0.4.3" = "sha256-aYFNOVGl2Iiw8/u1NrL3ZprTt48OFpG9LKs1EwEAfms=";
-              };
+      forEachSystem (system:
+        let
+          name = "musium";
+          version = builtins.substring 0 8 self.lastModifiedDate;
+          pkgs = import nixpkgs { inherit system; };
+        in
+          {
+            devShells.default = pkgs.mkShell {
+              inherit name;
+              nativeBuildInputs = [
+                pkgs.mkdocs
+                pkgs.psc-package
+                pkgs.purescript
+                pkgs.rustup
+                pkgs.sqlite
+                querybinder.packages.${system}.default
+              ];
             };
-            nativeBuildInputs = [ pkgs.pkg-config ];
-            buildInputs = [ pkgs.sqlite pkgs.alsa-lib ];
-          };
-        });
-      };
+
+            packages.default = pkgs.rustPlatform.buildRustPackage {
+              inherit name version;
+              src = ./.;
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+                outputHashes = {
+                  "claxon-0.4.3" = "sha256-aYFNOVGl2Iiw8/u1NrL3ZprTt48OFpG9LKs1EwEAfms=";
+                };
+              };
+              nativeBuildInputs = [ pkgs.pkg-config ];
+              buildInputs = [ pkgs.sqlite pkgs.alsa-lib ];
+            };
+          }
+      );
 }
