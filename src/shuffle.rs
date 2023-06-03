9dbf3a427642cbd35460e837b2a25ff06c55d651
Ruud van Asseldonk <dev@veniogames.com>
2023-06-03 21:08:31 +0200

    Make shuffling testable through a trait

--- src/shuffle.rs
+++ src/shuffle.rs
@@ -9,9 +9,9 @@
 
 use std::cmp;
 use std::collections::HashMap;
-use std::hash::Hash;
 use std::iter;
 
+// TODO: Replace with Nanorand.
 use rand::seq::SliceRandom;
 use rand::Rng;
 
@@ -25,16 +25,76 @@ type Prng = rand_chacha::ChaCha8Rng;
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 struct TrackRef(pub u32);
 
-fn shuffle(
-    index: &MemoryMetaIndex,
+/// Trait to decouple metadata lookups from shuffling.
+///
+/// This is to make the shuffling easier to test without having to construct
+/// full `QueuedTrack` instances and a full index.
+trait Shuffle {
+    type Track;
+
+    fn get_album_id(&self, track: &Self::Track) -> AlbumId;
+    fn get_artist_id(&self, album_id: AlbumId) -> ArtistId;
+}
+
+/// Shuffle implementation that is actually used in the server.
+impl Shuffle for MemoryMetaIndex {
+    type Track = QueuedTrack;
+
+    fn get_album_id(&self, track: &QueuedTrack) -> AlbumId {
+        track.track_id.album_id()
+    }
+
+    fn get_artist_id(&self, album_id: AlbumId) -> ArtistId {
+        // For "artist", we take the first album of the album artists. Two
+        // alternatives come to mind: counting every collaboration as a unique
+        // artist (more smaller groups), or counting every connected component
+        // in the graph of artists with edges for collaboration albums (fewer
+        // larger groups). If we make artists "more distinct", then we risk
+        // placing their tracks consecutively in the final order because we
+        // consider them distinct. If we make artists "less distinct", then we
+        // risk having too few of them to properly interleave. So one artist per
+        // album is probably okay, but also, it’s just the easiest thing to
+        // implement.
+        let album = self.get_album(album_id).expect("Queued tracks should exist on album.");
+        let artist_ids = self.get_album_artists(album.artist_ids);
+        artist_ids[0]
+    }
+}
+
+/// Shuffler for use in tests.
+///
+/// In the tests we use `u32` as the track type and abuse it a bit such that:
+/// * The least significant 8 bits identify the track on the album.
+/// * The next 8 bits identify the album within the artist.
+/// * The next 8 bits identify the artist.
+/// * The most significant 8 bits are zero.
+///
+/// This makes it easy to construct such ids as literals without having to build
+/// up large dictionaries etc. It's also easy to fuzz.
+pub struct TestShuffler;
+
+impl Shuffle for TestShuffler {
+    type Track = u32;
+
+    fn get_album_id(&self, track: &u32) -> AlbumId {
+        AlbumId((*track >> 8) as u64)
+    }
+
+    fn get_artist_id(&self, album_id: AlbumId) -> ArtistId {
+        ArtistId(album_id.0 >> 8)
+    }
+}
+
+fn shuffle<Meta: Shuffle>(
+    meta: Meta,
     rng: &mut Prng,
-    tracks: &mut [QueuedTrack],
+    tracks: &mut [Meta::Track],
 ) {
     // First we partition all tracks into albums. Rather than moving around the
     // full QueuedTrack all the time, we store indices into the tracks slice.
     let mut albums = HashMap::<AlbumId, Vec<TrackRef>>::new();
     for (i, track) in tracks.iter().enumerate() {
-        let album_id = track.track_id.album_id();
+        let album_id = meta.get_album_id(track);
         albums.entry(album_id).or_default().push(TrackRef(i as u32));
     }
 
@@ -45,20 +105,11 @@ fn shuffle(
         album_tracks.shuffle(rng);
     }
 
-    // Then we group everything back on artist. For "artist", we take the first
-    // album of the album artists. Two alternatives come to mind: counting every
-    // collaboration as a unique artist (more smaller groups), or counting every
-    // connected component in the graph of artists with edges for collaboration
-    // albums (fewer larger groups). If we make artists "more distinct", then we
-    // risk placing their tracks consecutively in the final order because we
-    // consider them distinct. If we make artists "less distinct", then we risk
-    // having too few of them to properly interleave. So one artist per album is
-    // probably okay, but also, it’s just the easiest thing to implement.
+    // Then we group everything back on artist.
     let mut artists = HashMap::<ArtistId, Vec<Vec<TrackRef>>>::new();
     for (album_id, album_tracks) in albums {
-        let album = index.get_album(album_id).expect("Queued tracks should exist on album.");
-        let artist_ids = index.get_album_artists(album.artist_ids);
-        artists.entry(artist_ids[0]).or_default().push(album_tracks);
+        let artist_id = meta.get_artist_id(album_id);
+        artists.entry(artist_id).or_default().push(album_tracks);
     }
 
     // Then we combine all albums into one partition per artist, using our
@@ -76,8 +127,8 @@ fn shuffle(
 
 fn shuffle_interleave(rng: &mut Prng, mut partitions: Vec<Vec<TrackRef>>) -> Vec<TrackRef> {
     // Shuffle partitions and then use a stable sort to sort by ascending
-    // length. This way, for partitions that are the same size, the ties are
-    // broken randomly.
+    // length. This way, for partitions that are the same size, the merge order
+    // is random, which aids the randomness of our shuffle.
     partitions.shuffle(rng);
     partitions.sort_by_key(|v| v.len());
 
