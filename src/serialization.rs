c4075e0b2cac4612cbe3a28fd000e485eddeff4e
Ruud van Asseldonk <dev@ruuda.nl>
2026-09-23 01:04:34 +0200

    Adjust the frecency score with another median
    I think it makes sense if half the album is in "overlistened" territory,
    and half in "underlistened". But the magnitude matters of course, I only
    really count it as overlistened if it's above 1.0, and underlistened if
    it's below -1.0. Anything in between is balanced enough.
    
    There is more to tweak for this normalization factor, but for now I am
    already pretty happy with this. I can suggest tracks to listen to, and
    tracks to not listen to.
    
    I should output some statistics also about how current liked and loved
    tracks are over/underlistened, so I can tweak the factors so that they
    end up roughly half on each side, then it's calibrated to my current
    library.

--- src/serialization.rs
+++ src/serialization.rs
@@ -132,13 +132,14 @@ pub fn write_album_json<W: Write>(
         serde_json::to_writer(&mut w, index.get_string(kv.track.artist))?;
         write!(
             w,
-            r#","duration_seconds":{},"rating":{},"ft0":{:.3},"ft1":{:.3},"fc0":{:.3},"fc1":{:.3}}}"#,
+            r#","duration_seconds":{},"rating":{},"ft0":{:.3},"ft1":{:.3},"fc0":{:.3},"fc1":{:.3},"off":{:.3}}}"#,
             kv.track.duration_seconds,
             score.rating as i8,
             score.ft0,
             score.ft1,
             score.fc0,
             score.fc1,
+            score.off,
         )?;
         first = false;
     }
--- src/user_data.rs
+++ src/user_data.rs
@@ -27,7 +27,7 @@ use std::convert::TryFrom;
 
 use crate::album_table::AlbumTable;
 use crate::database as db;
-use crate::playcount::{AlbumData, CountData, PlayCounter, PlayCounts, TimeVector, TrackData};
+use crate::playcount::{AlbumData, CountData, PlayCounter, PlayCounts, RevNotNan, TimeVector, TrackData};
 use crate::prim::{AlbumId, TrackId, TrackWithId};
 use crate::MemoryMetaIndex;
 
@@ -87,6 +87,7 @@ pub struct TrackScore {
     pub ft1: f32,
     pub fc0: f32,
     pub fc1: f32,
+    pub off: f32,
     pub rating: Rating,
 }
 
@@ -195,18 +196,21 @@ impl UserData {
         let has_b_side = n_disc1 >= (tracks.len() * 2 / 3);
 
         let mut result = Vec::with_capacity(tracks.len());
+        let mut buffer = Vec::with_capacity(tracks.len());
+
         for t in tracks {
             let counts = self.track_data.get(&t.track_id).cloned().unwrap_or_default();
             let rating = self.track_ratings.get(&t.track_id).cloned().unwrap_or_default();
 
             // We adjust the target playcount based on the track rating. A liked
             // track should be played about 2.7 times as much as a regular one,
-            // a loved one 2.7 times as much as a liked one, and a disliked one
-            // only 1/20 as much. We also add a penalty for B-sides, and for
-            // very short tracks.
+            // a loved one even more, and a disliked one only 1/20 as much.
+            // These numbers are tweaked by eyeballing the output across many
+            // of my albums and adjusting until it feels right. We also add a
+            // penalty for B-sides, and for very short tracks.
             let is_b_side = has_b_side && t.track_id.disc_number() != 1;
             let multiplier = match rating {
-                Rating::Love => 1.0 / 7.389,
+                Rating::Love => 1.0 / 6.000,
                 Rating::Like => 1.0 / 2.718,
                 Rating::Neutral if is_b_side => 4.0,
                 Rating::Neutral if t.track.duration_seconds < 60 => 4.0,
@@ -219,9 +223,43 @@ impl UserData {
                 ft1: counts.playcount_recently,
                 fc0: counts.playcount_longterm * multiplier,
                 fc1: counts.playcount_recently * multiplier,
+                off: 0.0,
                 rating,
             };
+            buffer.push(RevNotNan(score.fc0));
             result.push(score);
+        }
+
+        // Compute the median of the adjusted longterm playcounts. This is the
+        // "target" per track for this album.
+        let median_longterm = median(&mut buffer);
+        buffer.clear();
+
+        // Subtract the median from the longterm playcount, so we get a sense of
+        // whether this track is overplayed or underplayed, and then normalize
+        // to the log of the median playcount. Underplayed by 1 listen when we
+        // listened to most trackf 20 times already, is ~noise. But underplayed
+        // when we listened to most tracks only once, is real signal. I thought
+        // at first log might be too aggressive and use sqrt, but that one is
+        // too tame, then values rarely go over or under 1.
+        let norm = (median_longterm + 0.35).sqrt().recip();
+        //let norm = (2.7 + median_longterm).ln().recip();
+        for score in result.iter_mut() {
+            score.off = score.fc0 * norm;
+
+            // Then add the unadjusted recent playcount to it. Recent plays
+            // restore the balance on the longterm excess/deficit at least for
+            // now; if a track is underplayed by 3 listens, we don't want to
+            // listen to it 3 times in a row to compensate, we'll listen to it
+            // again in a few weeks.
+            score.off += score.ft1;
+
+            buffer.push(RevNotNan(score.off));
+        }
+
+        let median_offset = median(&mut buffer);
+        for score in result.iter_mut() {
+            score.off -= median_offset;
         }
 
         result
@@ -233,5 +271,14 @@ impl UserData {
     pub fn set_counts(&mut self, counts: CountData) {
         self.track_data = counts.tracks;
         self.album_data = counts.albums;
+    }
+}
+
+/// Compute the median of a non-empty buffer. Sorts the buffer.
+fn median(buffer: &mut Vec<RevNotNan>) -> f32 {
+    buffer.sort();
+    match buffer.len() / 2 {
+        n if 2 * n != buffer.len() => buffer[n].0,
+        n => 0.5 * (buffer[n].0 + buffer[n - 1].0),
     }
 }
